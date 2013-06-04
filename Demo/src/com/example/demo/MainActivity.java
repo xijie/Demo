cecaf50140c36b158e33195efa1c23f446bd6b16
xijie <xjjacy@gmail.com>
2013-06-04 09:45:17 +0800

    add text

--- Demo/src/com/example/demo/MainActivity.java
+++ Demo/src/com/example/demo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TextView text = (TextView) findViewById(R.id.text);
 	}
 
 	@Override
